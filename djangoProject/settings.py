102e30e38aa361f294ef1365429d1d0b9dc40fc2
shahzaib-raza <www.shahzaibraza881@gmail.com>
2022-01-22 19:23:55 +0500

    ..

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -117,11 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'pakwheels-analytics-app.git\static'
+STATIC_URL = '/static/'
 
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)),
-STATIC_ROOT = 'pakwheels-analytics-app.git\static'
-
+STATIC_ROOT = '/static_root'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/static/',
