4459703a021878eecb1c8970b2e5a7a777315c11
shahzaib-raza <www.shahzaibraza881@gmail.com>
2022-01-22 17:39:45 +0500

    changed in settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)),
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)),
 STATIC_ROOT = os.path.join(PROJECT_DIR[0][:len(PROJECT_DIR[0])-13], 'static')
 
 STATICFILES_DIRS = [
