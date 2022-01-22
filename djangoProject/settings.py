598046493e9edef7b2fd8a4b321e637ac0ed0253
shahzaib-raza <www.shahzaibraza881@gmail.com>
2022-01-23 00:15:50 +0500

    Signed-off-by: shahzaib-raza <www.shahzaibraza881@gmail.com>

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -127,6 +127,11 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/static/',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
