c86d6d2c6011ce5d5b4c2a1a6f50bc08a8e1acac
shahzaib-raza <www.shahzaibraza881@gmail.com>
2022-01-22 18:57:55 +0500

    edited

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'git.heroku.com/pakwheels-analytics-app.git/static'
 
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)),
 STATIC_ROOT = '/static/'
