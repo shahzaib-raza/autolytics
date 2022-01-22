e8c116145cc8dcdbb18de82eebb073c8772c81b7
shahzaib-raza <www.shahzaibraza881@gmail.com>
2022-01-22 17:29:48 +0500

    updated setting

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)),
+STATIC_ROOT = os.path.join(PROJECT_DIR[0], 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
