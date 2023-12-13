c7775b9b980788594c5aea6294fd08e457670d23
Shahzaib Raza <64272560+shahzaib-raza@users.noreply.github.com>
2023-12-13 21:56:08 +0500

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!$odrzs)sqx59k50a+a=2^(cktd!)&#m81q5z&997l+m5bk-23
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vercel.app', '*']
 
 # Application definition
 
@@ -81,12 +81,15 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
